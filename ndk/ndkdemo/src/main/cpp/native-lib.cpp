d04e2d20c5a2e108e20859f06425873fef8914e1
JasonJan <jason1211241203@gmail.com>
2019-09-24 23:23:21 +0800

    add ndk demo2

--- ndk/ndkdemo/src/main/cpp/native-lib.cpp
+++ ndk/ndkdemo/src/main/cpp/native-lib.cpp
@@ -1,5 +1,6 @@
-#include <jni.h>
+#include <jan_jason_ndkdemo_NdkDemoActivity.h>
 #include <string>
+#include <jni.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 
@@ -8,5 +9,20 @@ Java_jan_jason_ndkdemo_NdkDemoActivity_stringFromJNI(JNIEnv* env,jobject) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_jan_jason_ndkdemo_NdkDemoActivity_accessField(JNIEnv *env, jobject instance) {
+
+    //获取类
+    jclass jcla=env->GetObjectClass(instance);
+
+    //获取类的成员变量showText的id
+    jfieldID jfieldId=env->GetFieldID(jcla,"showText","Ljava/lang/string;");
+
+    jstring after=env->NewStringUTF("Hello NDK");
+
+    env->SetObjectField(instance,jfieldId,after);
 }
 
--- ndk/ndkdemo/src/main/java/jan/jason/ndkdemo/NdkDemoActivity.java
+++ ndk/ndkdemo/src/main/java/jan/jason/ndkdemo/NdkDemoActivity.java
@@ -2,10 +2,12 @@ package jan.jason.ndkdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
-public class NdkDemoActivity extends AppCompatActivity {
+public class NdkDemoActivity extends AppCompatActivity implements View.OnClickListener{
 
     static {
         System.loadLibrary("native-lib");
@@ -13,7 +15,13 @@ public class NdkDemoActivity extends AppCompatActivity {
 
     public native String stringFromJNI();
 
+    public native void accessField();
+
+    public String showText="Hello World!";
     private TextView txtNdk;
+    private TextView resultTxt;
+    private Button btnChange;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +32,22 @@ public class NdkDemoActivity extends AppCompatActivity {
         initData();
     }
 
+    @Override
+    public void onClick(View v) {
+        accessField();
+    }
+
     private void initView(){
         txtNdk=(TextView) findViewById(R.id.amn_demo_txt1);
+        btnChange=(Button) findViewById(R.id.amn_demo_btn1);
+        resultTxt=(TextView) findViewById(R.id.amn_demo_txt2);
     }
 
     private void initData(){
         String text=stringFromJNI();
         txtNdk.setText(text);
+
+        btnChange.setOnClickListener(this);
     }
+
 }
